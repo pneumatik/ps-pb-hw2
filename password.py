911d98f1eda1842a9069900353741d07657c6313
pneumatik <45317465+pneumatik@users.noreply.github.com>
2020-11-22 13:14:26 +0300

    password

--- password.py
+++ password.py
@@ -2,44 +2,14 @@ password = input('Введите пароль: ')
 
 numeric = password.isnumeric()
 blank = len(password)
-pss = 1
-#result = 'Привет'
-
-
-# try:
-#     pss = float(pss)   
-# except ValueError:
-#     print('OK')
 
 try: # проверяем только числа
-    numeric = 1 / (1 - numeric)
-    #print(result)
-    
+    numeric = 1 / (1 - numeric)    
 except ZeroDivisionError:
     print('Ваш пароль состоит только из цифр')
-    
-
-try: # проверяем пустой пароль
-    blank = 1 / blank
-except ZeroDivisionError:
+try: 
+    blank = 1 / blank + int(password)
+except ZeroDivisionError: # проверяем пустой пароль
      print('Вы ввели пустой пароль')
-
-try: # все в порядке
-    pss = bool(password)
-
-except ValueError:
-    print('OK')
-
-
-
-
-# print('OK')
-# print(password)ghgkhjkh
-# print('pss ' + str(pss))
-#
-#print('цифра ' + str(numeric))
-# print('пусто ' + str(blank))
-
-
-
-
+except ValueError: # проверяем отсутсвие проблем
+    print('Требования к паролю соблюдены')
